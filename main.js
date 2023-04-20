df6eb0625f0da044624cf3ea34fd8bad828185f3
MirheydarA <a.mirheyder004@gmail.com>
2023-04-20 11:40:50 +0400

    custom functions updated

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
-// Capitalize //
+//  Capitalize  //
 
-const str = 'i have learned something new today';
-const words = str.split(" ");
-for (var i = 0; i < words.length; i++) {
-    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1) ;
-}
-const str2 = words.join(" ");
-console.log(str2);
+// const str = 'i have learned something new today';
+// const words = str.split(" ");
+// for (var i = 0; i < words.length; i++) {
+//     words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+// }
+// const str2 = words.join(" ");
+// console.log(str2);
 
 
-// Reverse //
+//  Reverse  //
 
+function CustomReverseString(str) {
+    if (str == "")
+      return "";
+    else
+      return CustomReverseString(str.slice(1)) + str.charAt(0);
+  }
+  
+  console.log(CustomReverseString("Hez"));
+
+//  CapitalizeVowels  //
+
+const vowels = ["a", "e", "i", "o", "u"]
